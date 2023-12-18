4cac76d289797b869308ccc60f414393cfa719cb
John Izzard <johnny.drazzi@gmail.com>
2023-12-18 18:11:50 +1100

    Update UART library

--- USB_Stack/Examples/CDC_Examples/CDC_Serial_UART_Example.X/UART.c
+++ USB_Stack/Examples/CDC_Examples/CDC_Serial_UART_Example.X/UART.c
@@ -1,317 +1,367 @@
 /*
- * File:   UART.c
+ * File:   uart.c
  * Author: John Izzard
  *
  * Year:   2016
  */
 
-#include <xc.h>
-#include "UART.h"
+#define UART_SPBRG_CALC(freq, baud, div) (((float)(((freq / baud) / div) - 1)) + 0.5)
+#define UART_SPBRG_CALC_8(freq, baud) UART_SPBRG_CALC(freq, baud, 16)
+#define UART_SPBRG_CALC_16(freq, baud) UART_SPBRG_CALC(freq, baud, 4)
 
-#ifdef PIC32MX795F512L
-#define UART1_CALC (((((float)CLOCK_FREQ/UART1_BAUD)/4)-1)+0.5)
-/* ************************* UART1 ************************* */
-void UART1_Init(void) {
-    uint16_t x;
-    double PBCLK = _XTAL_FREQ;
-
-    TRISFbits.TRISF8 = 1;
-    TRISFbits.TRISF8 = 1;
-
-    U1MODEbits.BRGH = 1;// High rate
-
-    U1BRG = (uint16_t)UART1_CALC;
-
-    U1STAbits.UTXEN = 1; // Enable transmitter
-    U1STAbits.URXEN = 1; // Enable Receiver
-
-    U1MODEbits.ON = 1; // Enable serial port
-}
-uint8_t UART1_Data_Ready(){
-    if(IFS0bits.U1RXIF){IFS0bits.U1RXIF=0;return 1;}
-    else{return 0;}
-}
-uint8_t UART1_Tx_Idle(){
-    if(U1STAbits.TRMT){return 1;}
-    else{return 0;}
-}
-uint8_t UART1_Read(){
-    uint8_t temp;
-    if(U1STAbits.OERR){
-                        temp = U1RXREG;
-                        U1STAbits.OERR = 0;
-
-                        return 0;
-                       }
-    if(U1STAbits.FERR||U1STAbits.PERR){
-                        temp = U1RXREG;
-
-                        return 0;
-                       }
-
-    return U1RXREG;
-}
-void UART1_Read_Text(uint8_t *Output, uint8_t *Delimiter, uint8_t Attempts){
-    uint8_t i;
-    uint8_t x = 0;
-    uint16_t del_address = (uint16_t)Delimiter;
-    uint16_t out_address = (uint16_t)Output;
-
-    while(1){                       // Clears all elements in Output array
-        if(*Output==0){break;}      // until it sees the first Null character
-        else{*Output = 0; Output++;}
-    }
-    Output = (uint8_t *)out_address;   // Set pointer back to start of Output array
-
-    if(Attempts != 255){
-        for(i=0;i<Attempts;i++,Output++){
-
-            while(!UART1_Data_Ready()){}
-            *Output = UART1_Read();
-
-            if(*Output==*Delimiter){Delimiter++; x++; if(*Delimiter==0){break;}}
-            else{Delimiter=(uint8_t *)del_address;}
-        }
-    }
-    else{
-        while(1){
-
-            while(!UART1_Data_Ready()){}
-            *Output = UART1_Read();
-
-            if(*Output==*Delimiter){Delimiter++; x++; if(*Delimiter==0){break;}}
-            else{Delimiter=(uint8_t *)del_address;}
-        }
-    }
-
-    for(i=x;i!=0;i--,Output--){*Output=0;} // Remove delimiter
-}
-void UART1_Write(uint8_t data_){
-    while(!UART1_Tx_Idle()){};
-    U1TXREG = data_;
-}
-void UART1_Write_Text(uint8_t *UART_text){
-    while(*UART_text){
-        UART1_Write(*UART_text++);
-    }
-}
-#endif
-
-#ifdef __XC8
-#if NUM_UARTS >= 1
-/* ************************* UART1 ************************* */
 #ifdef BAUD_8BITS
-#define UART1_CALC (((((float)CLOCK_FREQ/UART1_BAUD)/16)-1)+0.5)
+#define UART1_SPBRG_DEFAULT UART_SPBRG_CALC_8(_XTAL_FREQ, UART1_BAUD)
+#define UART2_SPBRG_DEFAULT UART_SPBRG_CALC_8(_XTAL_FREQ, UART2_BAUD)
 #else
-#define UART1_CALC (((((float)CLOCK_FREQ/UART1_BAUD)/4)-1)+0.5)
-#define UART1_CALC_H (UART1_CALC/256)
+#define UART1_SPBRG_DEFAULT UART_SPBRG_CALC_16(_XTAL_FREQ, UART1_BAUD)
+#define UART2_SPBRG_DEFAULT UART_SPBRG_CALC_16(_XTAL_FREQ, UART2_BAUD)
+#define UART1_SPBRGH_DEFAULT (UART1_SPBRG_DEFAULT / 256)
+#define UART2_SPBRGH_DEFAULT (UART2_SPBRG_DEFAULT / 256)
 #endif /* BAUD_8BITS */
-void UART1_Init(void) {
-#ifdef HAS_PPS1
+
+#include <xc.h>
+#include "uart.h"
+
+/* STATIC PROTOTYPES */
+static void    init1(void);
+static void    set_baud1(uint16_t baud_calc);
+static bool    data_ready1(void);
+static bool    tx_idle1(void);
+static uint8_t read1(void);
+static void    write1(uint8_t byte);
+
+static void    init2(void);
+static void    set_baud2(uint16_t baud_calc);
+static bool    data_ready2(void);
+static bool    tx_idle2(void);
+static uint8_t read2(void);
+static void    write2(uint8_t byte);
+
+static void    read_string(uint8_t* output, uint8_t* delimiter, uint8_t attempts, bool (*data_ready)(void), uint8_t (*read)(void));
+static void    write_string(uint8_t* string, bool (*tx_idle)(void), void (*write)(uint8_t));
+
+
+/* GLOBAL FUNCTIONS */
+void uart__init(uint8_t uart)
+{
+    switch(uart)
+    {
+        case 0:
+            init1();
+            break;
+        #if NUM_UARTS >= 2
+        case 1:
+            init2();
+            break;
+        #endif
+        default:
+            break; // This uart doesn't exist.
+    }
+}
+
+void uart__set_baud(uint8_t uart, uint16_t baud)
+{
+    #if BAUD_8BITS
+    uint16_t baud_calc = (uint16_t)UART_SPBRG_CALC_8(_XTAL_FREQ, baud);
+    #else
+    uint16_t baud_calc = (uint16_t)UART_SPBRG_CALC_16(_XTAL_FREQ, baud);
+    #endif
+
+    switch(uart)
+    {
+        case 0:
+            set_baud1(baud_calc);
+            break;
+        #if NUM_UARTS >= 2
+        case 1:
+            set_baud2(baud_calc);
+            break;
+        #endif
+        default:
+            break; // This uart doesn't exist.
+    }
+}
+
+bool uart__data_ready(uint8_t uart)
+{
+    switch(uart)
+    {
+        case 0:
+            return data_ready1();
+        #if NUM_UARTS >= 2
+        case 1:
+            return data_ready2();
+        #endif
+        default:
+            return true; // This uart doesn't exist, return true.
+    }
+}
+
+bool uart__tx_idle(uint8_t uart)
+{
+    switch(uart)
+    {
+        case 0:
+            return tx_idle1();
+        #if NUM_UARTS >= 2
+        case 1:
+            return tx_idle2();
+        #endif
+        default:
+            return true; // This uart doesn't exist, return true.
+    }
+}
+
+uint8_t uart__read(uint8_t uart)
+{
+    switch(uart)
+    {
+        case 0:
+            return read1();
+        #if NUM_UARTS >= 2
+        case 1:
+            return read2();
+        #endif
+        default:
+            return 0; // This uart doesn't exist, returning something.
+    }
+}
+
+void uart__read_string(uint8_t uart, uint8_t* output, uint8_t* delimiter, uint8_t attempts)
+{
+    switch(uart)
+    {
+        case 0:
+            read_string(output, delimiter, attempts, data_ready1, read1);
+            break;
+        #if NUM_UARTS >= 2
+        case 1:
+            read_string(output, delimiter, attempts, data_ready2, read2);
+            break;
+        #endif
+        default:
+            break; // This uart doesn't exist.
+    }
+}
+
+void uart__write(uint8_t uart, uint8_t byte)
+{
+    switch(uart)
+    {
+        case 0:
+            write1(byte);
+            break;
+        #if NUM_UARTS >= 2
+        case 1:
+            write2(byte);
+            break;
+        #endif
+        default:
+            break; // This uart doesn't exist.
+    }
+}
+
+void uart__write_string(uint8_t uart, uint8_t* string)
+{
+    switch(uart)
+    {
+        case 0:
+            write_string(string, tx_idle1, write1);
+            break;
+        #if NUM_UARTS >= 2
+        case 1:
+            write_string(string, tx_idle2, write2);
+            break;
+        #endif
+        default:
+            break;  // This uart doesn't exist.
+    }
+}
+
+/* STATIC FUNCTIONS */
+static void init1(void)
+{
+    #ifdef HAS_PPS1
     PPSLOCK = PPS_UNLOCKED;
     PPS_RX1_REG = PPS_RX1_VAL;
     PPS_TX1_REG = PPS_TX1_VAL;
     PPSLOCK = PPS_LOCKED;
-#endif
+    #endif /* HAS_PPS1 */
     UART1_TX_TRIS = 1;
     UART1_RX_TRIS = 1;
 
-    TXSTAbits.BRGH = 1; // High rate
-    SPBRG = (uint8_t)UART1_CALC;
+    TXSTAbits.BRGH = 1; // High rate.
+    SPBRG = (uint8_t)UART1_SPBRG_DEFAULT;
     
-#ifdef BAUD_16BITS
-    BAUDCONbits.BRG16 = 1; // 16-bit baud rate generator
-    SPBRGH = (uint8_t)UART1_CALC_H;
-#endif
+    #ifdef BAUD_16BITS
+    BAUDCONbits.BRG16 = 1; // 16-bit baud rate generator.
+    SPBRGH = (uint8_t)UART1_SPBRGH_DEFAULT;
+    #endif /* BAUD_16BITS */
 
     TXSTAbits.SYNC = 0;
-    TXSTAbits.TXEN = 1; // Enable transmitter
-    RCSTAbits.CREN = 1; // Enable Receiver
-    RCSTAbits.SPEN = 1; // Enable serial port
+    TXSTAbits.TXEN = 1; // Enable transmitter.
+    RCSTAbits.CREN = 1; // Enable Receiver.
+    RCSTAbits.SPEN = 1; // Enable serial port.
 }
-uint8_t UART1_Data_Ready(void){
-    if(PIR1bits.RCIF){return 1;}
-    else{return 0;}
+
+static void set_baud1(uint16_t baud_calc)
+{
+    SPBRG = (uint8_t)baud_calc;
+    #ifdef BAUD_16BITS
+    SPBRGH = (uint8_t)(baud_calc >> 8);
+    #endif
 }
-uint8_t UART1_Tx_Idle(void){
-    if(TXSTAbits.TRMT){return 1;}
-    else{return 0;}
+
+static bool data_ready1(void)
+{
+    return PIR1bits.RCIF;
 }
-uint8_t UART1_Read(void){
+
+static bool tx_idle1(void)
+{
+    return TXSTAbits.TRMT;
+}
+
+static uint8_t read1(void)
+{
     volatile uint8_t temp;
-    if(RCSTAbits.OERR){
+    if(RCSTAbits.OERR)
+    {
+        // Clear buffer overrun error by reading RCREG.
         temp = RCREG;
         RCSTAbits.CREN = 0;
         RCSTAbits.CREN = 1;
         return 0;
     }
-    if(RCSTAbits.FERR){
-        while(RCSTAbits.FERR){temp = RCREG;}
+    if(RCSTAbits.FERR)
+    {
+        // Clear framing error by reading RCREG until FERR is cleared.
+        while(RCSTAbits.FERR) temp = RCREG;
         return 0;
     }
 
     return RCREG;
 }
-void UART1_Read_Text(uint8_t *Output, uint8_t *Delimiter, uint8_t Attempts){
-    uint8_t i;
-    uint8_t x = 0;
-    uint16_t del_address = (uint16_t)Delimiter;
-    uint16_t out_address = (uint16_t)Output;
 
-    while(1){                       // Clears all elements in Output array
-        if(*Output==0){break;}      // until it sees the first Null character
-        else{*Output = 0; Output++;}
-    }
-    Output = (uint8_t *)out_address;   // Set pointer back to start of Output array
-
-    if(Attempts != 255){
-        for(i=0;i<Attempts;i++,Output++){
-
-            while(!UART1_Data_Ready()){}
-            *Output = UART1_Read();
-
-            if(*Output==*Delimiter){Delimiter++; x++; if(*Delimiter==0){break;}}
-            else{Delimiter=(uint8_t *)del_address;}
-        }
-    }
-    else{
-        while(1){
-
-            while(!UART1_Data_Ready()){}
-            *Output = UART1_Read();
-
-            if(*Output==*Delimiter){Delimiter++; x++; if(*Delimiter==0){break;}}
-            else{Delimiter=(uint8_t *)del_address;}
-        }
-    }
-
-    for(i=x;i!=0;i--,Output--){*Output=0;} // Remove delimiter
+static void write1(uint8_t byte)
+{
+    TXREG = byte;
 }
-void UART1_Write(uint8_t data_){
-    while(!UART1_Tx_Idle()){};
-    TXREG = data_;
-}
-void UART1_Write_Const(const uint8_t data_){
-    while(!UART1_Tx_Idle()){};
-    TXREG = data_;
-}
-void UART1_Write_Text(uint8_t *UART_text){
-    while(*UART_text){
-        UART1_Write(*UART_text++);
-    }
-}
-void UART1_Write_Const_Text(const uint8_t *UART_text){
-    while(*UART_text){
-        UART1_Write_Const(*UART_text++);
-    }
-}
-#endif
+
 #if NUM_UARTS >= 2
-/* ************************* UART2 ************************* */
-#ifdef BAUD_8BITS
-#define UART2_CALC (((((float)CLOCK_FREQ/UART2_BAUD)/16)-1)+0.5)
-#else
-#define UART2_CALC (((((float)CLOCK_FREQ/UART2_BAUD)/4)-1)+0.5)
-#define UART2_CALC_H (UART2_CALC/256)
-#endif /* BAUD_8BITS */
-void UART2_Init(void) {
-#ifdef HAS_PPS2
+static void init2(void)
+{
+    #ifdef HAS_PPS2
     PPSLOCK = PPS_UNLOCKED;
     PPS_RX2_REG = PPS_RX2_VAL;
     PPS_TX2_REG = PPS_TX2_VAL;
     PPSLOCK = PPS_LOCKED;
-#endif
+    #endif /* HAS_PPS2 */
     UART2_TX_TRIS = 1;
     UART2_RX_TRIS = 1;
 
-    TXSTA2bits.BRGH = 1; // High rate
-    SPBRG2 = (uint8_t)UART2_CALC;
+    TXSTA2bits.BRGH = 1; // High rate.
+    SPBRG2 = (uint8_t)UART2_SPBRG_DEFAULT;
     
-#ifdef BAUD_16BITS
-    BAUDCON2bits.BRG16 = 1; // 16-bit baud rate generator
-    SPBRGH2 = (uint8_t)UART2_CALC_H;
-#endif
+    #ifdef BAUD_16BITS
+    BAUDCON2bits.BRG16 = 1; // 16-bit baud rate generator.
+    SPBRGH2 = (uint8_t)UART2_SPBRGH_DEFAULT;
+    #endif /* BAUD_16BITS */
 
     TXSTA2bits.SYNC = 0;
-    TXSTA2bits.TXEN = 1; // Enable transmitter
-    RCSTA2bits.CREN = 1; // Enable Receiver
-    RCSTA2bits.SPEN = 1; // Enable serial port
+    TXSTA2bits.TXEN = 1; // Enable transmitter.
+    RCSTA2bits.CREN = 1; // Enable Receiver.
+    RCSTA2bits.SPEN = 1; // Enable serial port.
 }
-uint8_t UART2_Data_Ready(void){
-    if(PIR3bits.RC2IF){return 1;}
-    else{return 0;}
+
+static void set_baud2(uint16_t baud_calc)
+{
+    SPBRG2 = (uint8_t)baud_calc;
+    #ifdef BAUD_16BITS
+    SPBRGH2 = (uint8_t)(baud_calc >> 8);
+    #endif
 }
-uint8_t UART2_Tx_Idle(void){
-    if(TXSTA2bits.TRMT){return 1;}
-    else{return 0;}
+
+static bool data_ready2(void)
+{
+    return PIR3bits.RC2IF;
 }
-uint8_t UART2_Read(void){
+
+static bool tx_idle2(void)
+{
+    return TXSTA2bits.TRMT;
+}
+
+static uint8_t read2(void)
+{
     volatile uint8_t temp;
-    if(RCSTA2bits.OERR){
+    if(RCSTA2bits.OERR)
+    {
+        // Clear buffer overrun error by reading RCREG.
         temp = RCREG2;
         RCSTA2bits.CREN = 0;
         RCSTA2bits.CREN = 1;
         return 0;
     }
-    if(RCSTA2bits.FERR){
-        while(RCSTA2bits.FERR){temp = RCREG1;}
+    if(RCSTA2bits.FERR)
+    {
+        // Clear framing error by reading RCREG until FERR is cleared.
+        while(RCSTA2bits.FERR) temp = RCREG2;
         return 0;
     }
 
     return RCREG2;
 }
-void UART2_Read_Text(uint8_t *Output, uint8_t *Delimiter, uint8_t Attempts){
-    uint8_t i;
-    uint8_t x = 0;
-    uint16_t del_address = (uint16_t)Delimiter;
-    uint16_t out_address = (uint16_t)Output;
 
-    while(1){                       // Clears all elements in Output array
-        if(*Output==0){break;}      // until it sees the first Null character
-        else{*Output = 0; Output++;}
-    }
-    Output = (uint8_t *)out_address;             // Set pointer back to start of Output array
-
-    if(Attempts != 255){
-        for(i=0;i<Attempts;i++,Output++){
-
-            while(!UART2_Data_Ready()){}
-            *Output = UART2_Read();
-
-            if(*Output==*Delimiter){Delimiter++; x++; if(*Delimiter==0){break;}}
-            else{Delimiter=(uint8_t *)del_address;}
-        }
-    }
-    else{
-        while(1){
-
-            while(!UART2_Data_Ready()){}
-            *Output = UART2_Read();
-
-            if(*Output==*Delimiter){Delimiter++; x++; if(*Delimiter==0){break;}}
-            else{Delimiter=(uint8_t *)del_address;}
-        }
-    }
-
-    for(i=x;i!=0;i--,Output--){*Output=0;} // Remove delimiter
-}
-void UART2_Write(uint8_t data_){
-    while(!UART2_Tx_Idle()){};
-    TXREG2 = data_;
-}
-void UART2_Write_Const(const uint8_t data_){
-    while(!UART2_Tx_Idle()){};
-    TXREG2 = data_;
-}
-void UART2_Write_Text(uint8_t *UART_text){
-    while(*UART_text){
-        UART2_Write(*UART_text++);
-    }
-}
-void UART2_Write_Const_Text(const uint8_t *UART_text){
-    while(*UART_text){
-        UART2_Write_Const(*UART_text++);
-    }
+static void write2(uint8_t byte)
+{
+    TXREG2 = byte;
 }
 #endif /* NUM_UARTS >= 2 */
-#endif /* __XC8 */
+
+static void read_string(uint8_t* output, uint8_t* delimiter, uint8_t attempts, bool (*data_ready)(void), uint8_t (*read)(void))
+{
+    uint8_t  del_i = 0;
+    uint8_t* del_start = delimiter;
+    uint8_t* out_start = output;
+    bool     del_found = false;
+    
+    // Clears all elements in output array.
+    while(*output) *(output++) = 0;
+    
+    // Set pointer back to start of output array.
+    output = out_start;
+
+    for(uint8_t i = 1; i <= attempts; i++, output++)
+    {
+        while(!data_ready()){}
+        *output = read();
+
+        if(*output == *delimiter)
+        {
+            delimiter++;
+            del_i++;
+            if(*delimiter == 0)
+            {
+                del_found = true;
+                break;
+            }
+        }
+        else delimiter = del_start;
+    }
+
+    if(del_found)
+    {
+        while(del_i--) *(output--) = 0; // Remove delimiter.
+    }
+    else out_start[0] = 0; // Return an empty string.
+}
+
+static void write_string(uint8_t* string, bool (*tx_idle)(void), void (*write)(uint8_t))
+{
+    while(*string)
+    {
+        while(!tx_idle()){}
+        write(*string++);
+    }
+}
--- USB_Stack/Examples/CDC_Examples/CDC_Serial_UART_Example.X/UART.h
+++ USB_Stack/Examples/CDC_Examples/CDC_Serial_UART_Example.X/UART.h
@@ -1,5 +1,5 @@
 /*
- * File:   UART.h
+ * File:   uart.h
  * Author: John Izzard
  *
  * Year:   2016
@@ -8,118 +8,25 @@
 #ifndef UART_H
 #define UART_H
 
-#define UART_Init             UART1_Init
-#define UART_Data_Ready       UART1_Data_Ready
-#define UART_Tx_Idle          UART1_Tx_Idle
-#define UART_Read             UART1_Read
-#define UART_Read_Text        UART1_Read_Text
-#define UART_Write            UART1_Write
-#define UART_Write_Const      UART1_Write_Const
-#define UART_Write_Text       UART1_Write_Text
-#define UART_Write_Const_Text UART1_Write_Const_Text
+#ifdef	__cplusplus
+extern "C" {
+#endif /* __cplusplus */
 
-/* ********* SETTINGS ********** */
-#define CLOCK_FREQ 48000000
-#define UART1_BAUD 9600
-#define UART2_BAUD 9600
-
-#if defined(_16F1454) || defined(_16F1455)
-#define BAUD_16BITS
-#define NUM_UARTS 1
-#define UART1_TX_TRIS TRISCbits.TRISC4
-#define UART1_RX_TRIS TRISCbits.TRISC5
-#elif defined(_16F1459)
-#define BAUD_16BITS
-#define NUM_UARTS 1
-#define UART1_TX_TRIS TRISBbits.TRISB7
-#define UART1_RX_TRIS TRISBbits.TRISB5
-#elif defined(_16F87) || defined(_16F88)
-#define BAUD_8BITS
-#define NUM_UARTS 1
-#define UART1_TX_TRIS TRISBbits.TRISB5
-#define UART1_RX_TRIS TRISBbits.TRISB2
-#elif defined(_16F18313)
-#define BAUD_16BITS
-#define NUM_UARTS 1
-#define UART1_TX_TRIS TRISAbits.TRISA4
-#define UART1_RX_TRIS TRISAbits.TRISA5
-#define HAS_PPS1
-#define PPS_LOCKED   0
-#define PPS_UNLOCKED 1
-#define PPS_RX1_REG RXPPS
-#define PPS_RX1_VAL 5      // RX is on RA5
-#define PPS_TX1_REG RA4PPS // TX is on RA5
-#define PPS_TX1_VAL 0x14
-#elif defined(_18F25K22)
-#define BAUD_16BITS
-#define NUM_UARTS 2
-#define UART1_TX_TRIS TRISCbits.TRISC6
-#define UART1_RX_TRIS TRISCbits.TRISC7
-#define UART2_TX_TRIS TRISBbits.TRISB6
-#define UART2_RX_TRIS TRISBbits.TRISB7
-#elif defined(_18F45K22)
-#define BAUD_16BITS
-#define NUM_UARTS 2
-#define UART1_TX_TRIS TRISCbits.TRISC6
-#define UART1_RX_TRIS TRISCbits.TRISC7
-#define UART2_TX_TRIS TRISDbits.TRISD6
-#define UART2_RX_TRIS TRISDbits.TRISD7
-#elif defined(_18F47J53_FAMILY_) || defined(_18F46J50_FAMILY_)
-#define BAUD_16BITS
-#define NUM_UARTS 2
-#define UART1_TX_TRIS TRISCbits.TRISC6
-#define UART1_RX_TRIS TRISCbits.TRISC7
-#define UART2_TX_TRIS TRISBbits.TRISB6
-#define UART2_RX_TRIS TRISBbits.TRISB7
-#define HAS_PPS2
-#define PPSLOCK PPSCONbits.IOLOCK
-#define PPS_LOCKED   1
-#define PPS_UNLOCKED 0
-#define PPS_RX2_REG RPINR16
-#define PPS_RX2_VAL 10    // RX2 is on RB7 (RP10)
-#define PPS_TX2_REG RPOR9 // TX2 is on RB6 (RP9)
-#define PPS_TX2_VAL 6
-#elif defined(_18F13K50) || defined(_18F14K50)
-#define BAUD_16BITS
-#define NUM_UARTS 1
-#define UART1_TX_TRIS TRISBbits.TRISB7
-#define UART1_RX_TRIS TRISBbits.TRISB5
-#elif defined(_18F24K50) || defined(_18F25K50) || defined(_18F45K50) || \
-      defined(_18F4520_FAMILY_) || defined(_18F4450_FAMILY_) || defined(_18F4550_FAMILY_)
-#define BAUD_16BITS
-#define NUM_UARTS 1
-#define UART1_TX_TRIS TRISCbits.TRISC6
-#define UART1_RX_TRIS TRISCbits.TRISC7
-#else
-#error UART - DEVICE NOT YET SUPPORTED
-#endif
-
+#include "uart_settings.h"
 #include <stdint.h>
+#include <stdbool.h>
 
-#if NUM_UARTS >= 1
-/* *********** UART1 ************ */
-void UART1_Init(void);
-uint8_t UART1_Data_Ready(void);
-uint8_t UART1_Tx_Idle(void);
-uint8_t UART1_Read(void);
-void UART1_Read_Text(uint8_t *Output, uint8_t *Delimiter, uint8_t Attempts);
-void UART1_Write(uint8_t data_);
-void UART1_Write_Const(const uint8_t data_);
-void UART1_Write_Text(uint8_t *UART_text);
-void UART1_Write_Const_Text(const uint8_t *UART_text);
-#endif
+void    uart__init(uint8_t uart);
+void    uart__set_baud(uint8_t uart, uint16_t baud);
+bool    uart__data_ready(uint8_t uart);
+bool    uart__tx_idle(uint8_t uart);
+uint8_t uart__read(uint8_t uart);
+void    uart__read_string(uint8_t uart, uint8_t* output, uint8_t* delimiter, uint8_t attempts);
+void    uart__write(uint8_t uart, uint8_t byte);
+void    uart__write_string(uint8_t uart, uint8_t* string);
 
-#if NUM_UARTS >= 2
-/* *********** UART2 ************ */
-void UART2_Init(void);
-uint8_t UART2_Data_Ready(void);
-uint8_t UART2_Tx_Idle(void);
-uint8_t UART2_Read(void);
-void UART2_Read_Text(uint8_t *Output, uint8_t *Delimiter, uint8_t Attempts);
-void UART2_Write(uint8_t data_);
-void UART2_Write_Const(const uint8_t data_);
-void UART2_Write_Text(uint8_t *UART_text);
-void UART2_Write_Const_Text(const uint8_t *UART_text);
-#endif
+#ifdef	__cplusplus
+}
+#endif /* __cplusplus */
 
 #endif /* UART_H */
